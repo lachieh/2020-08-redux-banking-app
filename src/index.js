a6764d3b9e21b9e82afeed83f18fa7738cab1962
Lachlan Heywood <lachlan@heywood.net.au>
2020-11-10 16:32:33 -0500

    update with inital redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+console.log('Starting Banking App');
+
+const defaultState = {
+  balance: 0
+}
+
+const actionIncrement = {
+  type: 'INCREMENT',
+  payload: {
+    amount: 1
+  }
+}
+
+const actionDecrement = {
+  type: 'DECREMENT',
+  payload: {
+    amount: 1
+  }
+}
+
+function increment(num) {
+  return {
+    type: 'INCREMENT',
+    payload: {
+      amount: num
+    }
+  }
+}
+
+function decrement(num) {
+  return {
+    type: 'DECREMENT',
+    payload: {
+      amount: num
+    }
+  }
+}
+
+// function accountReducer(state=defaultState, action) {
+//   if (action.type === 'INCREMENT') {
+//     return {
+//       balance: state.balance + 1
+//     }
+//   }
+//   if (action.type === 'DECREMENT') {
+//     return {
+//       balance: state.balance - 1
+//     }
+//   }
+//   return state;
+// }
+
+function accountReducer(state=defaultState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        balance: state.balance + action.payload.amount
+      }
+    case 'DECREMENT':
+      return {
+        balance: state.balance - action.payload.amount
+      }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(accountReducer);
+
+store.subscribe(() => {
+  console.log('~~~~ State updated! ~~~~')
+  const state = store.getState();
+  console.log(state);
+})
+
+window.store = store;
+window.increment = increment;
+window.decrement = decrement;
+window.actionIncrement = actionIncrement;
+window.actionDecrement = actionDecrement;
+
+
+
+
+
+
+
+
+
+
+
